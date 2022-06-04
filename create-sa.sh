870047f252316f6a0d1e405e408295091e9c82aa
hugo <hugof_lacerda@hotmail.com>
2022-06-04 18:11:12 +0100

    Script to create storage account for final project

---
+++ create-sa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## Make sure to have azure cli installed and perform 'az login' before executing the script
+
+RESOURCE_GROUP_NAME='pgsicp-rg'
+STORAGE_ACCOUNT_NAME='pgsicpsa'
+CONTAINER_NAME='container-tfstates'
+
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location northeurope
+
+# Create storage account, cheapest and less redundant option LRS=Locally redundant storage
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
